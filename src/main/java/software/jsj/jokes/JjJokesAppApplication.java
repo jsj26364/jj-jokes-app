114da5873127cef518cc21b1d01ec75d57b49fa4
jsj26364 <jsj26364@gmail.com>
2019-07-17 22:01:18 +1000

    Initial Commit

---
+++ src/main/java/software/jsj/jokes/JjJokesAppApplication.java
@@ -0,0 +1,13 @@
+package software.jsj.jokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjJokesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjJokesAppApplication.class, args);
+	}
+
+}
