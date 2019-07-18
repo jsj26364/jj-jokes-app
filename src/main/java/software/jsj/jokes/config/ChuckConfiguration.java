375bc680c8c9a7ce659c0b141f04c27919629b42
jsj26364 <jsj26364@gmail.com>
2019-07-19 00:48:59 +1000

    configure ChuckNorrisQuotes bean

---
+++ src/main/java/software/jsj/jokes/config/ChuckConfiguration.java
@@ -0,0 +1,14 @@
+package software.jsj.jokes.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+@Configuration
+public class ChuckConfiguration {
+
+  @Bean
+  public ChuckNorrisQuotes chuckNorrisQuotes() {
+    return new ChuckNorrisQuotes();
+  }
+}
